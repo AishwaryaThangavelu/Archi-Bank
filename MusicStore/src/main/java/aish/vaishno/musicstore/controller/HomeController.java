5dcd5b7f5f269869e5948a116dc2dc0f4b540e0d
Aishwarya Thangavelu <aishwaryathangavelu@gmail.com>
2013-07-11 10:33:15 +0530

    FirstCommit

---
+++ MusicStore/src/main/java/aish/vaishno/musicstore/controller/HomeController.java
@@ -0,0 +1,46 @@
+package aish.vaishno.musicstore.controller;
+
+import aish.vaishno.musicstore.dao.IMusicStoreDao;
+import aish.vaishno.musicstore.pojo.MusicDetails;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+        @Autowired
+        private IMusicStoreDao musicStoreDao;
+    
+	@RequestMapping(value="/")
+	public ModelAndView test(HttpServletResponse response) throws IOException{
+            
+		return new ModelAndView("home","musicDetForm",new MusicDetails());
+	}
+        
+        @RequestMapping(value="AddSong",method = RequestMethod.POST)
+        @ResponseBody
+        public String addSong(@ModelAttribute("musicDetForm") MusicDetails musicDetails){
+           return musicStoreDao.addSong(musicDetails);
+        }
+        
+        @RequestMapping("SongList/")
+        @ResponseBody
+        public List<MusicDetails> getSongList(){
+            return musicStoreDao.getSongList();
+        }
+        
+//        @RequestMapping(value = "particularSong/")
+//        public MusicDetails getParticularSongDetails(Integer musicID){
+//            
+//        }
+}
